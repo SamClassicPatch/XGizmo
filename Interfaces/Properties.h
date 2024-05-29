a3b39387db737161115734c8ccc0cb0518fb9376
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2024-05-30 02:18:15 +0300

    Add Data, Properties, Render and World interfaces with methods.

---
+++ Interfaces/Properties.h
@@ -0,0 +1,196 @@
+/* Copyright (c) 2022-2024 Dreamy Cecil
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
+#ifndef XGIZMO_INCL_PROPERTIESINTERFACE_H
+#define XGIZMO_INCL_PROPERTIESINTERFACE_H
+
+#ifdef PRAGMA_ONCE
+  #pragma once
+#endif
+
+#include <Engine/Entities/Entity.h>
+#include <Engine/Entities/EntityProperties.h>
+
+// Interface of useful methods for entity property manipulation
+namespace IProperties {
+
+// Narrow down the easily editable property type
+inline INDEX ConvertType(CEntityProperty::PropertyType eProperty)
+{
+  ASSERT(eProperty > 0 && eProperty < 27);
+
+  static const INDEX aiPropertyTypes[27] = {
+    -1, // Invalid type
+    CEntityProperty::EPT_INDEX,
+    CEntityProperty::EPT_INDEX,
+    CEntityProperty::EPT_FLOAT,
+    CEntityProperty::EPT_INDEX,
+    CEntityProperty::EPT_STRING,
+    CEntityProperty::EPT_FLOAT,
+    CEntityProperty::EPT_ENTITYPTR,
+    CEntityProperty::EPT_STRING,
+    CEntityProperty::EPT_INDEX,
+    CEntityProperty::EPT_INDEX,
+    CEntityProperty::EPT_INDEX,
+    -1, // EPT_FLOATAABBOX3D
+    CEntityProperty::EPT_FLOAT,
+    CEntityProperty::EPT_FLOAT3D,
+    CEntityProperty::EPT_FLOAT3D,
+    -1, // EPT_FLOATplane3D
+    -1, // EPT_MODELOBJECT
+    -1, // EPT_PLACEMENT3D
+    -1, // EPT_ANIMOBJECT
+    CEntityProperty::EPT_STRING,
+    -1, // EPT_SOUNDOBJECT
+    CEntityProperty::EPT_STRING,
+    -1, // EPT_FLOATQUAT3D
+    -1, // EPT_FLOATMATRIX3D
+    CEntityProperty::EPT_INDEX,
+    -1, // EPT_MODELINSTANCE
+  };
+
+  return aiPropertyTypes[eProperty];
+};
+
+// Set property value from a pointer to the variable
+inline BOOL SetPropValue(CEntity *penEntity, CEntityProperty *pep, void *pValue) {
+  INDEX iPropType = ConvertType(pep->ep_eptType);
+  SLONG slOffset = pep->ep_slOffset;
+
+  switch (iPropType)
+  {
+    case CEntityProperty::EPT_INDEX: {
+      INDEX &iValue = ENTITYPROPERTY(penEntity, slOffset, INDEX);
+      iValue = *(INDEX *)pValue;
+    } break;
+
+    case CEntityProperty::EPT_FLOAT: {
+      FLOAT &fValue = ENTITYPROPERTY(penEntity, slOffset, FLOAT);
+      fValue = *(FLOAT *)pValue;
+    } break;
+
+    case CEntityProperty::EPT_STRING: {
+      CTString &strValue = ENTITYPROPERTY(penEntity, slOffset, CTString);
+      strValue = *(CTString *)pValue;
+    } break;
+
+    case CEntityProperty::EPT_ENTITYPTR: {
+      CEntityPointer &penPointer = ENTITYPROPERTY(penEntity, slOffset, CEntityPointer);
+      penPointer = *(CEntity **)pValue; // Pointer to entity pointer for consistency
+    } break;
+
+    case CEntityProperty::EPT_FLOAT3D: {
+      FLOAT3D &vValue = ENTITYPROPERTY(penEntity, slOffset, FLOAT3D);
+      vValue = *(FLOAT3D *)pValue;
+    } break;
+
+    // Invalid type
+    default: return FALSE;
+  }
+
+  // Value has been set
+  return TRUE;
+};
+
+// Get value from an entity property and write it into a variable by its pointer
+inline BOOL GetPropValue(CEntity *penEntity, CEntityProperty *pep, void **pValue) {
+  INDEX iPropType = ConvertType(pep->ep_eptType);
+  SLONG slOffset = pep->ep_slOffset;
+
+  switch (iPropType)
+  {
+    case CEntityProperty::EPT_INDEX: {
+      INDEX &iValue = ENTITYPROPERTY(penEntity, slOffset, INDEX);
+      *pValue = &iValue;
+    } break;
+
+    case CEntityProperty::EPT_FLOAT: {
+      FLOAT &fValue = ENTITYPROPERTY(penEntity, slOffset, FLOAT);
+      *pValue = &fValue;
+    } break;
+
+    case CEntityProperty::EPT_STRING: {
+      CTString &strValue = ENTITYPROPERTY(penEntity, slOffset, CTString);
+      *pValue = &strValue;
+    } break;
+
+    case CEntityProperty::EPT_ENTITYPTR: {
+      CEntityPointer &penPointer = ENTITYPROPERTY(penEntity, slOffset, CEntityPointer);
+      *pValue = (CEntity *)penPointer;
+    } break;
+
+    case CEntityProperty::EPT_FLOAT3D: {
+      FLOAT3D &vValue = ENTITYPROPERTY(penEntity, slOffset, FLOAT3D);
+      *pValue = &vValue;
+    } break;
+
+    // Invalid type
+    default: {
+      *pValue = NULL;
+    } return FALSE;
+  }
+
+  // Value has been retrieved
+  return TRUE;
+};
+
+// Get value of from an entity property of an index type
+inline BOOL GetIndexValue(CEntity *penEntity, CEntityProperty *pep, INDEX &iValue) {
+  void *pGetValue;
+  if (!GetPropValue(penEntity, pep, &pGetValue)) return FALSE;
+
+  iValue = *(INDEX *)pGetValue;
+  return TRUE;
+};
+
+// Get value of from an entity property of a float type
+inline BOOL GetFloatValue(CEntity *penEntity, CEntityProperty *pep, FLOAT &fValue) {
+  void *pGetValue;
+  if (!GetPropValue(penEntity, pep, &pGetValue)) return FALSE;
+
+  fValue = *(FLOAT *)pGetValue;
+  return TRUE;
+};
+
+// Get value of from an entity property of a string type
+inline BOOL GetStringValue(CEntity *penEntity, CEntityProperty *pep, CTString &strValue) {
+  void *pGetValue;
+  if (!GetPropValue(penEntity, pep, &pGetValue)) return FALSE;
+
+  strValue = *(CTString *)pGetValue;
+  return TRUE;
+};
+
+// Get value of from an entity property of an entity pointer type
+inline BOOL GetPointerValue(CEntity *penEntity, CEntityProperty *pep, CEntity *&penValue) {
+  void *pGetValue;
+  if (!GetPropValue(penEntity, pep, &pGetValue)) return FALSE;
+
+  penValue = (CEntity *)pGetValue;
+  return TRUE;
+};
+
+// Get value of from an entity property of a vector type
+inline BOOL GetVectorValue(CEntity *penEntity, CEntityProperty *pep, FLOAT3D &vValue) {
+  void *pGetValue;
+  if (!GetPropValue(penEntity, pep, &pGetValue)) return FALSE;
+
+  vValue = *(FLOAT3D *)pGetValue;
+  return TRUE;
+};
+
+}; // namespace
+
+#endif
---
+++ Interfaces/World.h
@@ -0,0 +1,281 @@
+/* Copyright (c) 2022-2024 Dreamy Cecil
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
+#ifndef XGIZMO_INCL_WORLDINTERFACE_H
+#define XGIZMO_INCL_WORLDINTERFACE_H
+
+#ifdef PRAGMA_ONCE
+  #pragma once
+#endif
+
+#include <Engine/Entities/Entity.h>
+#include <Engine/World/World.h>
+
+#include "../Entities/BaseClasses.h"
+
+// Interface of useful methods for world and entity manipulation
+namespace IWorld { 
+
+// Get current game world
+inline CWorld *GetWorld(void) {
+  return &_pNetwork->ga_World;
+};
+
+// Gather entities of the same class
+inline void GetEntitiesOfClass(CEntities &cInput, CEntities &cOutput, LibClassHolder lchClass) {
+  FOREACHINDYNAMICCONTAINER(cInput, CEntity, iten) {
+    CEntity *pen = &*iten;
+
+    if (pen->GetFlags() & ENF_DELETED) {
+      continue;
+    }
+
+    // Same class
+    if (pen->GetClass()->ec_pdecDLLClass == lchClass.pdec) {
+      cOutput.Add(pen);
+    }
+  }
+};
+
+// Find entity in a world by its ID
+inline CEntity *FindEntityByID(CWorld *pwo, const ULONG ulEntityID) {
+  FOREACHINDYNAMICCONTAINER(GetWorld()->wo_cenEntities, CEntity, iten) {
+    CEntity *pen = &*iten;
+
+    if (pen->GetFlags() & ENF_DELETED) {
+      continue;
+    }
+
+    // Same ID
+    if (pen->en_ulID == ulEntityID) {
+      return pen;
+    }
+  }
+
+  return NULL;
+};
+
+// Find existing entity property by its ID
+inline CEntityProperty *PropertyForId(LibClassHolder lch, ULONG ulID) {
+  CDLLEntityClass *pdec = lch;
+
+  while (pdec != NULL) {
+    // For each property
+    for (INDEX iProp = 0; iProp < pdec->dec_ctProperties; iProp++) {
+      CEntityProperty &ep = pdec->dec_aepProperties[iProp];
+
+      // Matching ID
+      if (ep.ep_ulID == ulID) {
+        return &ep;
+      }
+    }
+
+    // Next class in the hierarchy
+    pdec = pdec->dec_pdecBase;
+  }
+
+  return NULL;
+};
+
+// Find existing entity property by its name hash
+inline CEntityProperty *PropertyForHash(LibClassHolder lch, ULONG ulNameHash) {
+  CDLLEntityClass *pdec = lch;
+
+  while (pdec != NULL) {
+    // For each property
+    for (INDEX iProp = 0; iProp < pdec->dec_ctProperties; iProp++) {
+      CEntityProperty &ep = pdec->dec_aepProperties[iProp];
+
+      // Matching name hash
+      ULONG ulCheckHash = CTString(ep.ep_strName).GetHash();
+
+      if (ulCheckHash == ulNameHash) {
+        return &ep;
+      }
+    }
+
+    // Next class in the hierarchy
+    pdec = pdec->dec_pdecBase;
+  }
+
+  return NULL;
+};
+
+// Find entity property by its ID or offset of a specific type
+inline CEntityProperty *PropertyForIdOrOffset(LibClassHolder lch, ULONG ulType, ULONG ulID, SLONG slOffset) {
+  CDLLEntityClass *pdec = lch;
+
+  while (pdec != NULL) {
+    // For each property
+    for (INDEX iProp = 0; iProp < pdec->dec_ctProperties; iProp++) {
+      CEntityProperty &ep = pdec->dec_aepProperties[iProp];
+
+      // Only check the matching type
+      if (ep.ep_eptType != ulType) continue;
+
+      // Matching ID or offset (ID is more likely to remain the same)
+      if (ep.ep_ulID == ulID || ep.ep_slOffset == slOffset) {
+        return &ep;
+      }
+    }
+
+    // Next class in the hierarchy
+    pdec = pdec->dec_pdecBase;
+  }
+
+  return NULL;
+};
+
+// Find entity property by its name of a specific type
+inline CEntityProperty *PropertyForName(LibClassHolder lch, ULONG ulType, const CTString &strName) {
+  CDLLEntityClass *pdec = lch;
+
+  while (pdec != NULL) {
+    // For each property
+    for (INDEX iProp = 0; iProp < pdec->dec_ctProperties; iProp++) {
+      CEntityProperty &ep = pdec->dec_aepProperties[iProp];
+
+      // Only check the matching type
+      if (ep.ep_eptType != ulType) continue;
+
+      // Matching name
+      if (strName == ep.ep_strName) {
+        return &ep;
+      }
+    }
+
+    // Next class in the hierarchy
+    pdec = pdec->dec_pdecBase;
+  }
+
+  return NULL;
+};
+
+// Find entity property by its name or ID of a specific type
+inline CEntityProperty *PropertyForNameOrId(LibClassHolder lch, ULONG ulType, const CTString &strName, ULONG ulID) {
+  // Find property by name first, if there's any
+  CEntityProperty *pep = (strName == "" ? NULL : PropertyForName(lch, ulType, strName));
+
+  // Try searching by ID
+  if (pep == NULL) {
+    pep = PropertyForIdOrOffset(lch, ulType, ulID, -1);
+  }
+
+  return pep;
+};
+
+// Find WorldSettingsController in a world
+inline CEntity *GetWSC(CWorld *pwo) {
+  CEntity *penBack = pwo->GetBackgroundViewer();
+
+  if (penBack != NULL) {
+    // Get property offset only once
+    static SLONG slPointerOffset = -1;
+
+    if (slPointerOffset == -1) {
+      // Obtain entity pointer property
+      CEntityProperty *pep = penBack->PropertyForName("World settings controller");
+
+      // No entity pointer
+      if (pep == NULL) return NULL;
+
+      slPointerOffset = pep->ep_slOffset;
+    }
+
+    // Obtain WorldSettingsController
+    return (CEntity *)ENTITYPROPERTY(penBack, slPointerOffset, CEntityPointer);
+  }
+
+  return NULL;
+};
+
+// Find entities of a specific class
+inline void FindClasses(CEntities &cInput, CEntities &cOutput, const char *strClass) {
+  ASSERT(cOutput.Count() == 0);
+
+  FOREACHINDYNAMICCONTAINER(cInput, CEntity, iten) {
+    CEntity *penCheck = iten;
+
+    if (!IsDerivedFromClass(penCheck, strClass)) {
+      continue;
+    }
+
+    cOutput.Add(penCheck);
+  }
+};
+
+// Find entities of a specific class ID
+inline void FindClassesByID(CEntities &cInput, CEntities &cOutput, INDEX iClassID) {
+  ASSERT(cOutput.Count() == 0);
+
+  FOREACHINDYNAMICCONTAINER(cInput, CEntity, iten) {
+    CEntity *penCheck = iten;
+
+    if (!IsDerivedFromID(penCheck, iClassID)) {
+      continue;
+    }
+
+    cOutput.Add(penCheck);
+  }
+};
+
+// Check if there are any local players
+inline BOOL AnyLocalPlayers(void)
+{
+  FOREACHINSTATICARRAY(_pNetwork->ga_aplsPlayers, CPlayerSource, itpls) {
+    // At least one player has been added
+    if (itpls->pls_Index >= 0) return TRUE;
+  }
+
+  // No local players (observers may be present)
+  return FALSE;
+};
+
+// Get pointers to local player entities
+inline void GetLocalPlayers(CPlayerEntities &cOutput) {
+  ASSERT(cOutput.Count() == 0);
+
+  // Go through all local players
+  FOREACHINSTATICARRAY(_pNetwork->ga_aplsPlayers, CPlayerSource, itpls) {
+    INDEX iPlayer = itpls->pls_Index;
+
+    // Player hasn't been added
+    if (iPlayer < 0) continue;
+
+    // Add player entity
+    CPlayerTarget &plt = _pNetwork->ga_sesSessionState.ses_apltPlayers[iPlayer];
+    cOutput.Add(plt.plt_penPlayerEntity);
+  }
+};
+
+// Get viewpoint of a player entity
+inline CPlacement3D GetViewpoint(CPlayerEntity *pen, BOOL bLerped) {
+  CPlacement3D plView;
+
+  if (bLerped) {
+    plView.Lerp(pen->en_plLastViewpoint, pen->en_plViewpoint, _pTimer->GetLerpFactor());
+    plView.RelativeToAbsoluteSmooth(pen->GetLerpedPlacement());
+
+  } else {
+    plView = pen->en_plViewpoint;
+    plView.RelativeToAbsoluteSmooth(pen->GetPlacement());
+  }
+
+  return plView;
+};
+
+}; // namespace
+
+#endif
