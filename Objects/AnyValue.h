52a14fd6a5a475fa262fd022e8744acade858de8
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2024-06-06 22:00:59 +0300

    Add AnyValue object for storing different values of specific types.

---
+++ Objects/AnyValue.h
@@ -0,0 +1,176 @@
+/* Copyright (c) 2024 Dreamy Cecil
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
+
+#ifndef XGIZMO_INCL_ANYVALUE_H
+#define XGIZMO_INCL_ANYVALUE_H
+
+#ifdef PRAGMA_ONCE
+  #pragma once
+#endif
+
+#include <algorithm>
+
+// Type-safe container for single values of any type
+class CAnyValue {
+  public:
+    // Supported types
+    enum EType {
+      E_NULL   = 0,
+      E_BOOL   = 1,
+      E_INDEX  = 2,
+      E_FLOAT  = 3,
+      E_STRING = 4,
+    };
+
+    // Value placeholder
+    struct Placeholder {
+      virtual ~Placeholder() {};
+      virtual EType GetType() const = 0;
+      virtual Placeholder *Clone() const = 0;
+    };
+
+    // Value holder of a specific type
+    template<class Type, EType eType>
+    struct Holder : public Placeholder {
+      Type _value;
+
+      Holder(const Type &valSet) : _value(valSet) {};
+      virtual EType GetType() const { return eType; };
+      virtual Placeholder *Clone() const { return new Holder(_value); };
+    };
+
+    typedef Holder<INDEX,    E_BOOL>   Bool_t;
+    typedef Holder<INDEX,    E_INDEX>  Int_t;
+    typedef Holder<FLOAT,    E_FLOAT>  Float_t;
+    typedef Holder<CTString, E_STRING> String_t;
+
+  private:
+    Placeholder *_content; // Currently held value
+
+  public:
+    // Default constructor
+    CAnyValue() : _content(NULL) {};
+
+    // Constructors from supported types
+    CAnyValue(bool  bSet) : _content(new Bool_t (bSet)) {};
+    CAnyValue(int   iSet) : _content(new Int_t  (iSet)) {};
+    CAnyValue(float fSet) : _content(new Float_t(fSet)) {};
+    CAnyValue(const CTString &strSet) : _content(new String_t(strSet)) {};
+    CAnyValue(const char     *strSet) : _content(new String_t(strSet)) {};
+
+    // Copy constructor
+    CAnyValue(const CAnyValue &other) : _content(!other.IsEmpty() ? other._content->Clone() : NULL) {};
+
+    // Destructor
+    ~CAnyValue() {
+      delete _content;
+    };
+
+  public:
+    // Get value type
+    inline EType GetType() const {
+      return !IsEmpty() ? _content->GetType() : E_NULL;
+    };
+
+    // Swap values
+    inline void Swap(CAnyValue &other) {
+      std::swap(_content, other._content);
+    };
+
+    // Assign a new value by swapping values with a temporary copy
+    inline CAnyValue &operator=(CAnyValue other) {
+      other.Swap(*this);
+      return *this;
+    };
+
+    // Check if value is empty
+    inline bool IsEmpty() const {
+      return _content == NULL;
+    };
+
+  // Value accessors
+  public:
+
+    inline INDEX &GetIndex(void) {
+      ASSERT(GetType() == E_BOOL || GetType() == E_INDEX);
+      return ((Int_t *)_content)->_value;
+    };
+
+    inline FLOAT &GetFloat(void) {
+      ASSERT(GetType() == E_FLOAT);
+      return ((Float_t *)_content)->_value;
+    };
+
+    inline CTString &GetString(void) {
+      ASSERT(GetType() == E_STRING);
+      return ((String_t *)_content)->_value;
+    };
+    
+  // Value converters
+  public:
+
+    inline bool IsTrue(void) const {
+      switch (GetType()) {
+        case E_BOOL:
+        case E_INDEX:  return ((Int_t    *)_content)->_value != 0;
+        case E_FLOAT:  return ((Float_t  *)_content)->_value != 0.0f;
+        case E_STRING: return ((String_t *)_content)->_value.Length() != 0;
+      }
+
+      ASSERTALWAYS("Unknown value type in CAnyValue::IsTrue()");
+      return false;
+    };
+
+    // Implicit conversion
+    __forceinline operator bool() const {
+      return IsTrue();
+    };
+
+    inline INDEX ToIndex(void) const {
+      switch (GetType()) {
+        case E_BOOL:
+        case E_INDEX: return ((Int_t   *)_content)->_value;
+        case E_FLOAT: return ((Float_t *)_content)->_value;
+      }
+
+      ASSERTALWAYS("Unknown value type in CAnyValue::ToIndex()");
+      return 0;
+    };
+
+    inline FLOAT ToFloat(void) const {
+      switch (GetType()) {
+        case E_BOOL:
+        case E_INDEX: return ((Int_t   *)_content)->_value;
+        case E_FLOAT: return ((Float_t *)_content)->_value;
+      }
+
+      ASSERTALWAYS("Unknown value type in CAnyValue::ToFloat()");
+      return 0.0f;
+    };
+
+    inline CTString ToString(void) const {
+      switch (GetType()) {
+        case E_BOOL:   return ((Int_t   *)_content)->_value ? "1" : "0";
+        case E_INDEX:  return CTString(0, "%d", ((Int_t   *)_content)->_value);
+        case E_FLOAT:  return CTString(0, "%g", ((Float_t *)_content)->_value);
+        case E_STRING: return ((String_t *)_content)->_value;
+      }
+
+      ASSERTALWAYS("Unknown value type in CAnyValue::ToString()");
+      return "";
+    };
+};
+
+#endif
